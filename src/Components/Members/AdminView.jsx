1423160ab5a0cc958270dfe439b30915facb510d
j0r1ss0u <jorisgalea@gmail.com>
2025-09-02 14:19:31 +0000

    02/09 - Lien Tool/Library/Forum

--- src/Components/Members/AdminView.jsx
+++ src/Components/Members/AdminView.jsx
@@ -584,7 +584,10 @@ const AdminView = () => {
                       <td className="px-3 sm:px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                         <div className="min-w-[100px] flex justify-end space-x-2">
                           <button
-                            onClick={() => setEditingMember(member)}
+                            onClick={() => {
+                              setEditingMember(member);
+                              setShowMemberForm(true);
+                            }}
                             className="text-indigo-600 hover:text-indigo-900"
                             title="Edit organization"
                           >
